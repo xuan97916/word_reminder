f9f01a3f882e3d0d28eb7bb1b8b5a22a7ee9e530
xuan5 <xuan5@jd.com>
2024-03-25 23:00:56 +0800

    first commit

---
+++ reminder.py
@@ -0,0 +1,31 @@
+
+
+word_dict = {}
+
+word_list = []
+
+def make_word_dict(word_path, word_dict, word_list):
+    with open(word_path, 'r') as f:
+        for line in f:
+            english = line.split(',')[0].strip()
+            chinese = line.split(',')[1].strip()
+            word_dict[english] = chinese
+            word_list.append(english)
+
+
+make_word_dict('./word_book/test_word_0.txt', word_dict, word_list)
+
+while len(word_list) > 0:
+    print(word_dict[word_list[0]])
+    answer = input('your answer: ')
+    if answer == word_list[0]:
+        print('correct! You have ' + str(len(word_list) - 1) + ' words left.')
+        word_list.pop(0)
+    else:
+        print('Wrong! The correct answer is ' + word_list[0] + '. You have ' + str(len(word_list)) + ' words left.')
+        wrong_word = word_list.pop(0)
+        word_list.append(wrong_word)
+
+
+
+
