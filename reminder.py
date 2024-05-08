9e338813aeade86320aa5235d5f4470f32ab46ef
xuan5 <xuan5@jd.com>
2024-05-08 22:15:42 +0800

    add whole word reminder

--- reminder.py
+++ reminder.py
@@ -1,4 +1,4 @@
-
+import os
 
 word_dict = {}
 
@@ -12,8 +12,18 @@ def make_word_dict(word_path, word_dict, word_list):
             word_dict[english] = chinese
             word_list.append(english)
 
+def make_word_dict_all(word_dir, word_dict, word_list):
+    file_list = [os.path.join(word_dir,x) for x in os.listdir(word_dir)]
+    for file_name in file_list: 
+        with open(file_name, 'r',encoding='UTF-8') as f:
+            for line in f:
+                english = line.split(',')[0].strip()
+                chinese = line.split(',')[1].strip()
+                word_dict[english] = chinese
+                word_list.append(english)
 
-make_word_dict('./word_book/test_word_4-1', word_dict, word_list)
+# make_word_dict('./word_book/test_word_3-7', word_dict, word_list)
+make_word_dict_all('./word_book', word_dict, word_list)
 
 while len(word_list) > 0:
     print(word_dict[word_list[0]])
